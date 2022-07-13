dc9cbcf055d44a524618b74fea3f3b8897336757
BarracudaPff <mrm.kikll@gmail.com>
2022-07-13 09:51:02 +0200

    M1L4 | rewrite with module-architecture

---
+++ m1l4-dsl/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "202206-ok-marketplace"
+
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
+
+include("m1l1-hello-world")
+include("m1l4-dsl")
