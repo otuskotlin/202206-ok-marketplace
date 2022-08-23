00a15d4b50cf687c84542ee1b686f80f3f574b5c
BarracudaPff <mrm.kikll@gmail.com>
2022-08-23 08:58:40 +0200

    M3L2 make ktor mlp

--- ok-marketplace-app-ktor/src/jvmMain/kotlin/ru/otus/otuskotlin/marketplace/v1/rest.kt
+++ ok-marketplace-app-ktor/src/jvmMain/kotlin/ru/otus/otuskotlin/marketplace/v1/rest.kt
@@ -2,8 +2,6 @@ package ru.otus.otuskotlin.marketplace.v1
 
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import ru.otus.otuskotlin.marketplace.backend.services.AdService
-import ru.otus.otuskotlin.marketplace.backend.services.OfferService
 
 fun Route.v1Ad() {
     route("ad") {
