0811383bcfabd55e25ca4826a2bb4b9d22a873ea
BarracudaPff <mrm.kikll@gmail.com>
2022-08-23 09:03:20 +0200

    M3L2 upd gradle files

--- ok-marketplace-app-ktor/build.gradle.kts
+++ ok-marketplace-app-ktor/build.gradle.kts
@@ -1,21 +1,132 @@
 import org.jetbrains.kotlin.util.suffixIfNot
 
 val ktorVersion: String by project
+val logbackVersion: String by project
+val serializationVersion: String by project
 
 // ex: Converts to "io.ktor:ktor-ktor-server-netty:2.0.1" with only ktor("netty")
 fun ktor(module: String, prefix: String = "server-", version: String? = this@Build_gradle.ktorVersion): Any =
     "io.ktor:ktor-${prefix.suffixIfNot("-")}$module:$version"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm")
     id("application")
     id("com.bmuschko.docker-java-application")
+    id("com.bmuschko.docker-remote-api")
     kotlin("plugin.serialization")
+    kotlin("multiplatform")
+}
+
+repositories {
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 application {
 //    mainClass.set("io.ktor.server.netty.EngineMain")
     mainClass.set("ru.otus.otuskotlin.marketplace.app.ApplicationKt")
+}
+
+kotlin {
+    jvm {}
+
+    val nativeTarget = when (System.getProperty("os.name")) {
+        "Mac OS X" -> macosX64("native")
+        "Linux" -> linuxX64("native")
+        // Windows is currently not supported
+        // Other supported targets are listed here: https://ktor.io/docs/native-server.html#targets
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "ru.otus.otuskotlin.marketplace.main"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(ktor("core")) // "io.ktor:ktor-server-core:$ktorVersion"
+
+                implementation(project(":ok-marketplace-common"))
+                implementation(project(":ok-marketplace-api-v2-kmp"))
+                implementation(project(":ok-marketplace-mappers-v2"))
+                implementation(project(":ok-marketplace-stubs"))
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val nativeMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-cio:$ktorVersion")
+
+                implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            }
+        }
+        val nativeTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation(ktor("core")) // "io.ktor:ktor-server-core:$ktorVersion"
+                implementation(ktor("netty")) // "io.ktor:ktor-ktor-server-netty:$ktorVersion"
+
+                // jackson
+                implementation(ktor("jackson", "serialization")) // io.ktor:ktor-serialization-jackson
+                implementation(ktor("content-negotiation")) // io.ktor:ktor-server-content-negotiation
+                implementation(ktor("kotlinx-json", "serialization")) // io.ktor:ktor-serialization-kotlinx-json
+
+                implementation(ktor("locations"))
+                implementation(ktor("caching-headers"))
+                implementation(ktor("call-logging"))
+                implementation(ktor("auto-head-response"))
+                implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
+                implementation(ktor("default-headers")) // "io.ktor:ktor-cors:$ktorVersion"
+                implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
+                implementation(ktor("auto-head-response"))
+                implementation(ktor("websockets"))
+
+                implementation(ktor("websockets")) // "io.ktor:ktor-websockets:$ktorVersion"
+                implementation(ktor("auth")) // "io.ktor:ktor-auth:$ktorVersion"
+                implementation(ktor("auth-jwt")) // "io.ktor:ktor-auth-jwt:$ktorVersion"
+
+                implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+                // transport models
+                implementation(project(":ok-marketplace-common"))
+                implementation(project(":ok-marketplace-api-v1-jackson"))
+                implementation(project(":ok-marketplace-api-v2-kmp"))
+                implementation(project(":ok-marketplace-mappers-v1"))
+
+                // v2 api
+                implementation(project(":ok-marketplace-mappers-v2"))
+
+                // Stubs
+                implementation(project(":ok-marketplace-stubs"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation(ktor("test-host")) // "io.ktor:ktor-server-test-host:$ktorVersion"
+                implementation(ktor("content-negotiation", prefix = "client-"))
+                implementation(ktor("websockets", prefix = "client-"))
+            }
+        }
+    }
 }
 
 docker {
@@ -35,50 +146,27 @@ docker {
     }
 }
 
-dependencies {
-    val logbackVersion: String by project
-    val kotlinLoggingJvmVersion: String by project
+tasks {
+    val linkReleaseExecutableNative by getting(org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink::class) {
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation(ktor("core")) // "io.ktor:ktor-server-core:$ktorVersion"
-    implementation(ktor("netty")) // "io.ktor:ktor-ktor-server-netty:$ktorVersion"
-
-    // jackson
-    implementation(ktor("jackson", "serialization")) // io.ktor:ktor-serialization-jackson
-    implementation(ktor("content-negotiation")) // io.ktor:ktor-server-content-negotiation
-    implementation(ktor("kotlinx-json", "serialization")) // io.ktor:ktor-serialization-kotlinx-json
-
-    implementation(ktor("locations"))
-    implementation(ktor("caching-headers"))
-    implementation(ktor("call-logging"))
-    implementation(ktor("auto-head-response"))
-    implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
-    implementation(ktor("default-headers")) // "io.ktor:ktor-cors:$ktorVersion"
-    implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
-    implementation(ktor("auto-head-response"))
-    implementation(ktor("websockets"))
-
-    implementation(ktor("websockets")) // "io.ktor:ktor-websockets:$ktorVersion"
-    implementation(ktor("auth")) // "io.ktor:ktor-auth:$ktorVersion"
-    implementation(ktor("auth-jwt")) // "io.ktor:ktor-auth-jwt:$ktorVersion"
-
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-
-    // transport models
-    implementation(project(":ok-marketplace-common"))
-    implementation(project(":ok-marketplace-app-ktor-common"))
-    implementation(project(":ok-marketplace-api-v1-jackson"))
-    implementation(project(":ok-marketplace-api-v2-kmp"))
-    implementation(project(":ok-marketplace-mappers-v1"))
-
-    // v2 api
-    implementation(project(":ok-marketplace-mappers-v2"))
-
-    // Stubs
-    implementation(project(":ok-marketplace-stubs"))
-
-    testImplementation(kotlin("test-junit"))
-    testImplementation(ktor("test-host")) // "io.ktor:ktor-server-test-host:$ktorVersion"
-    testImplementation(ktor("content-negotiation", prefix = "client-"))
-    testImplementation(ktor("websockets", prefix = "client-"))
+    }
+    val dockerDockerfile by creating(com.bmuschko.gradle.docker.tasks.image.Dockerfile::class) {
+        group = "docker"
+        from("ubuntu:22.02")
+        doFirst {
+            copy {
+                from(linkReleaseExecutableNative.binary.outputFile)
+                into("${this@creating.temporaryDir}/app")
+            }
+        }
+        copyFile("app", "/app")
+        entryPoint("/app")
+    }
+//    @Suppress("UNUSED_VARIABLE")
+//    val dockerBuildImage by creating(com.bmuschko.gradle.docker.tasks.image.DockerBuildImage::class) {
+//        group = "docker"
+//        dependsOn(dockerDockerfile)
+//        images.add("${project.name}:${project.version}")
+//    }
 }
+
