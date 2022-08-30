da5160e8ed64277d1df080ed513c0fdb378b6571
Evgeny Nepomnyaschy <evgeny.nepomnyaschy@mvideo.ru>
2022-08-30 09:01:25 +0300

    M4L2-s0 Инициализация модуля бизнес-логики

--- ok-marketplace-biz/build.gradle.kts
+++ ok-marketplace-biz/build.gradle.kts
@@ -2,26 +2,24 @@ plugins {
     kotlin("multiplatform")
 }
 
-group = rootProject.group
-version = rootProject.version
+repositories {
+    mavenCentral()
+}
 
 kotlin {
     jvm {}
-    macosX64 {}
-    linuxX64 {}
 
     sourceSets {
-        val datetimeVersion: String by project
-        val coroutinesVersion: String by project
 
+        @Suppress("UNUSED_VARIABLE")
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
 
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                api("org.jetbrains.kotlinx:kotlinx-datetime:$datetimeVersion")
+                implementation(project(":ok-marketplace-common"))
             }
         }
+        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
