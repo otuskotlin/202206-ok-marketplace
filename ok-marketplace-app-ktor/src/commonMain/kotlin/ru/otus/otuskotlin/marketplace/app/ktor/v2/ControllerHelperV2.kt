10fb42875ee152532286fbdf56e0fe8ef0b2e101
DTadescu <tada.mascot@gmail.com>
2022-11-05 22:07:41 +0300

    M8L2  Логирование в Кторе

--- ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/ktor/v2/ControllerHelperV2.kt
+++ ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/ktor/v2/ControllerHelperV2.kt
@@ -6,6 +6,7 @@ import io.ktor.server.response.*
 import kotlinx.datetime.Clock
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
+import ru.otus.otuskotlin.marketplace.api.logs.mapper.toLog
 import ru.otus.otuskotlin.marketplace.api.v2.apiV2Mapper
 import ru.otus.otuskotlin.marketplace.api.v2.models.IRequest
 import ru.otus.otuskotlin.marketplace.api.v2.models.IResponse
@@ -14,21 +15,34 @@ import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.common.helpers.asMkplError
 import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
 import ru.otus.otuskotlin.marketplace.common.models.MkplState
+import ru.otus.otuskotlin.marketplace.logging.common.IMpLogWrapper
 import ru.otus.otuskotlin.marketplace.mappers.v2.fromTransport
 import ru.otus.otuskotlin.marketplace.mappers.v2.toTransportAd
 
 suspend inline fun <reified Q : IRequest, @Suppress("unused") reified R : IResponse> ApplicationCall.processV2(
     processor: MkplAdProcessor,
+    logger: IMpLogWrapper,
+    logId: String,
     command: MkplCommand? = null,
 ) {
     val ctx = MkplContext(
         timeStart = Clock.System.now(),
     )
     try {
-        val request = apiV2Mapper.decodeFromString<Q>(receiveText())
-        ctx.fromTransport(request)
-        processor.exec(ctx)
-        respond(apiV2Mapper.encodeToString(ctx.toTransportAd()))
+        logger.doWithLogging(id = logId) {
+            val request = apiV2Mapper.decodeFromString<Q>(receiveText())
+            ctx.fromTransport(request)
+            logger.info(
+                msg = "$command request is got",
+                data = ctx.toLog("${logId}-got")
+            )
+            processor.exec(ctx)
+            logger.info(
+                msg = "$command request is handled",
+                data = ctx.toLog("${logId}-handled")
+            )
+            respond(apiV2Mapper.encodeToString(ctx.toTransportAd()))
+        }
     } catch (e: Throwable) {
         command?.also { ctx.command = it }
         ctx.state = MkplState.FAILING
--- ok-marketplace-app-ktor/src/jvmMain/kotlin/ru/otus/otuskotlin/marketplace/app/ktor/v1/OfferController.kt
+++ ok-marketplace-app-ktor/src/jvmMain/kotlin/ru/otus/otuskotlin/marketplace/app/ktor/v1/OfferController.kt
@@ -5,7 +5,8 @@ import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersRequest
 import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersResponse
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
 import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
+import ru.otus.otuskotlin.marketplace.logging.common.IMpLogWrapper
 
 
-suspend fun ApplicationCall.offersAd(processor: MkplAdProcessor) =
-    processV1<AdOffersRequest, AdOffersResponse>(processor, MkplCommand.OFFERS)
+suspend fun ApplicationCall.offersAd(processor: MkplAdProcessor, logger: IMpLogWrapper) =
+    processV1<AdOffersRequest, AdOffersResponse>(processor, logger, "ad-offers", MkplCommand.OFFERS)
