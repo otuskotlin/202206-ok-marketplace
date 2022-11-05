c1502b5ca55df4ea56bb908285e7211ca4a0bf2c
DTadescu <tada.mascot@gmail.com>
2022-11-05 22:39:59 +0300

    M8L2  Логирование в Spring

--- ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/api/v1/controller/ControllerHelperV1.kt
+++ ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/api/v1/controller/ControllerHelperV1.kt
@@ -1,6 +1,7 @@
 package ru.otus.otuskotlin.marketplace.springapp.api.v1.controller
 
 import kotlinx.datetime.Clock
+import ru.otus.otuskotlin.marketplace.api.logs.mapper.toLog
 import ru.otus.otuskotlin.marketplace.api.v1.models.IRequest
 import ru.otus.otuskotlin.marketplace.api.v1.models.IResponse
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
@@ -8,6 +9,7 @@ import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.common.helpers.asMkplError
 import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
 import ru.otus.otuskotlin.marketplace.common.models.MkplState
+import ru.otus.otuskotlin.marketplace.logging.common.IMpLogWrapper
 import ru.otus.otuskotlin.marketplace.mappers.v1.fromTransport
 import ru.otus.otuskotlin.marketplace.mappers.v1.toTransportAd
 
@@ -15,14 +17,26 @@ suspend inline fun <reified Q : IRequest, reified R : IResponse> processV1(
     processor: MkplAdProcessor,
     command: MkplCommand? = null,
     request: Q,
+    logger: IMpLogWrapper,
+    logId: String,
 ): R {
     val ctx = MkplContext(
         timeStart = Clock.System.now(),
     )
     return try {
-        ctx.fromTransport(request)
-        processor.exec(ctx)
-        ctx.toTransportAd() as R
+        logger.doWithLogging(id = logId) {
+            ctx.fromTransport(request)
+            logger.info(
+                msg = "$command request is got",
+                data = ctx.toLog("${logId}-got")
+            )
+            processor.exec(ctx)
+            logger.info(
+                msg = "$command request is handled",
+                data = ctx.toLog("${logId}-handled")
+            )
+            ctx.toTransportAd() as R
+        }
     } catch (e: Throwable) {
         command?.also { ctx.command = it }
         ctx.state = MkplState.FAILING
